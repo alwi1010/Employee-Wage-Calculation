c2364a0bb51c6aedaefea2cab1c1764a5f9803ad
Sayid Alwi Al-Attas <38781603+alwi1010@users.noreply.github.com>
2018-07-16 00:11:48 +0800

    Add files via upload

---
+++ src/utspbo2_012/UTSPBO2_012.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utspbo2_012;
+
+/**
+ *
+ * @author Pro Sawi
+ */
+public class UTSPBO2_012 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormGaji tampil = new FormGaji();
+        tampil.setVisible(true);
+        tampil.setLocationRelativeTo(null);
+    }
+    
+}
